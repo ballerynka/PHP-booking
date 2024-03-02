a9be779a488b194e0866a4460d0a569961665bf3
IrinaM <150373521+ballerynka@users.noreply.github.com>
2024-03-03 01:24:09 +0200

    Update index.php

--- booking/src/index.php
+++ booking/src/index.php
@@ -83,3 +83,82 @@ $bonusPlaces = 10;                               //camelCase
 
 
 --> 
+<hr>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<? 
+const TICKET_PRICE = 100.50;   // USD
+const DISCOUNT_L = 300.00;     // USD
+const DISCOUNT_P = 20;     // %
+
+
+$quantity = 4;   // tickets
+
+$total = $quantity * TICKET_PRICE;
+ 
+
+if ($total > DISCOUNT_L){
+  $total = $total * (100 - DISCOUNT_P) / 100;
+
+}
+
+print($total);
+
+
+
+
+?>
+<hr>
+<br><br><br><br><br><br><br><br>
+<!-- LOGIC -->
+
+<?
+ const TICKET_COST = 100;
+if (array_key_exists('quantity', $_GET)){
+  $quantity = $_GET['quantity'];
+
+  if (preg_match('/^\d+$/', $quantity)){
+    $quantity = (int) $quantity;
+   //$quantity = (int) $quantity;
+ $cost = TICKET_COST * $quantity;
+  $totalCost = $cost;
+  }
+  else{
+    $error ="Value must be an integer number";
+  }
+}
+
+else{
+$error = "You didn't specify any quantity";
+}
+
+
+
+?>
+<!-- TEMPLATE -->
+<br>
+<a href="/?quantity=1">Buy 1 ticket</a><br>
+<a href="/?quantity=2">Buy 2 tickets</a><br>
+<a href="/?quantity=3">Buy 3 tickets</a><br>
+<hr>
+
+<form method="GET" action="/">
+  <input type="text" name="quantity" placeholder="Enter desired value...">
+  <button>BUY</button>
+</form>
+
+<? if (isset($totalCost)): ?>
+<div><?= $quantity?> tickets x <?= TICKET_COST?> = <?= $totalCost?></div>
+<? endif ?>
+
+<? if (isset($error)): ?>
+<div style="color:red;"><?= $error?></div>
+<? endif ?>
+
