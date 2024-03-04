3ad723baf0344be5c1990fecd54e2c41d734de51
IrinaM <150373521+ballerynka@users.noreply.github.com>
2024-03-05 00:07:02 +0200

    Update grade-calculator.php

--- booking/src/examples/grade-calculator.php
+++ booking/src/examples/grade-calculator.php
@@ -25,65 +25,40 @@ button,td:last-of-type{
 
 </style>
 
-
-
 <?
 
-if (isset($_GET['grade1'], $_GET['grade2'], $_GET['grade3'] )){
-    $grade1 = $_GET['grade1'];
-    $grade2 = $_GET['grade2'];
-    $grade3 = $_GET['grade3'];
+if(isset($_GET['grade1'],$_GET['grade2'],$_GET['grade2'])){
+        $grade1 = $_GET['grade1'];
+        $grade2 = $_GET['grade2'];
+        $grade3 = $_GET['grade3'];
 
-    if(is_numeric($_GET['grade1'])){
-      
-        if ($grade1 = filter_var($grade1) >= 0 && $grade1 <= 10){
-          
-    }else{
-        $error = "only from 0 to 10";
-    }
-
-}else{
-        $error2 = 'You must enter only numeric symbols';
-    }       
-
-   
-    if(is_numeric($_GET['grade2'])){
-        if ($grade2 = filter_var($grade2) >= 0 && $grade2 <= 10){
-            
-              
-      }else{
-          $error = "only from 0 to 10";
-      }
-  
-  }else{
-          $error2 = 'You must enter only numeric symbols';
-      }       
-      if(is_numeric($_GET['grade3'])){
-        if ($grade3 = filter_var($grade3) >= 0 && $grade3 <= 10){
-             
-            
-      }else{
-          $error = "only from 0 to 10";
-      }
-  
-  }else{
-          $error2 = 'You must enter only numeric symbols';
-      }       
-      $grade1 = (float) $_GET['grade1'];
-      $grade2 = (float) $_GET['grade2'];
-      $grade3 = (float) $_GET['grade3'];
-      $average = ($grade1 + $grade2 + $grade3) / 3;
 }
 
-    
 
+
+    if(is_numeric($_GET['grade1']) && is_numeric($_GET['grade2']) && is_numeric($_GET['grade3'])){
+  
+    }else{
+        $error = 'You can enter only numeric symbols';
+        
+    }
+ 
+
+
+
+    if ($grade1 = filter_var($grade1) >= 0 && $grade1 <= 10 && $grade2 = filter_var($grade2) >= 0 && $grade2 <= 10 && $grade3 = filter_var($grade3) >= 0 && $grade3 <= 10) {
+        $grade1 = (float)$_GET['grade1'];
+        $grade2 = (float)$_GET['grade2'];
+        $grade3 = (float)$_GET['grade3'];
+        $average =  ((float)$_GET['grade1'] + (float)$_GET['grade2']+ (float)$_GET['grade3']) / 3;
+    }else{
+        $error2 = "You can enter only numbers from 0 to 10";}
+        
 ?>
 
-<br>
-<br>
-<br>
 
-<form action="/examples/grade-calculator.php">
+
+<form action="/examples/grade-calculator2.php" method="GET">
     <input type="text" name="grade1" required>
     <input type="text" name="grade2" required>
     <input type="text" name="grade3" required>
@@ -91,35 +66,19 @@ if (isset($_GET['grade1'], $_GET['grade2'], $_GET['grade3'] )){
 </form>
 
 
-<br>
-<br>
-<br>
-
-
-<?if(isset($average)):?>
-   
-<table>
-
-<tr>
-<td><?print($grade1)?></td>
-<td><?print($grade2)?></td>
-<td><?print($grade3)?></td>
-
-
-<?if(isset($average)):?>
-<td><?=$average?></td>
-<? endif?>
-
-
-</tr>
-</table>
-
-<?if(isset($error)):?><?=$error?>
-<? endif?>
-
-
-<?if(isset($error2)):?><?=$error2?>
-<? endif?>
-  
-
-
+    <?if(isset($average)):?>
+<table><tr>
+<td><?= ($grade1)?></td>
+<td><?= ($grade2)?></td>
+<td><?= ($grade3)?></td>
+<td><?= ($average)?></td></tr></table>
+    <?endif?>
+    <?if(isset($error)){
+    print($error);
+}else{
+    if(isset($error2)):
+ print($error2);
+    endif;
+    }
+?>
+ 
